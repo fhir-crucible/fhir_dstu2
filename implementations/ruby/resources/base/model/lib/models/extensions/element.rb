6f7e2cd68ffbd3e729869060a22dbac0147736cc
Jason Walonoski <jwalonoski@mitre.org>
2015-09-29 14:54:19 -0400

    Fix primitive extensions again.

--- implementations/ruby/resources/base/model/lib/models/extensions/element.rb
+++ implementations/ruby/resources/base/model/lib/models/extensions/element.rb
@@ -11,13 +11,20 @@ module FHIR
     end
 
     def has_primitive_extension?(field,index=0)
-      pe = primitiveExtension.select{|pe|pe.path=="_#{field}"}.first
-      !pe.nil? && !pe['extension'].nil? && !pe['extension'][index].nil?
+      pe = primitiveExtension.select{|pe|pe.path=="_#{field}"}.try(:first)
+      has_in_hash = !pe.nil? && !pe['extension'].nil? && !pe['extension'][index].nil?
+      has_as_attr = !pe.nil? && !pe.read_attribute(:extension).nil? && !pe.read_attribute(:extension)[index].nil?
+      (has_in_hash || has_as_attr)
     end
 
     def get_primitive_extension(field,index=0)
-      pe = primitiveExtension.select{|pe|pe.path=="_#{field}"}.first
-      pe = pe['extension'][index] if pe && pe['extension'] && pe['extension'][index]
+      pe = primitiveExtension.select{|pe|pe.path=="_#{field}"}.try(:first)
+      if pe && pe['extension'] && pe['extension'][index]
+        pe = pe['extension'][index] 
+      elsif pe && pe.read_attribute(:extension) && pe.read_attribute(:extension)[index]
+        pe = pe.read_attribute(:extension)[index] 
+      end
+      pe
     end
 
   end
